fd5d4263128d03238b1a49b211495307888318a1
Maria101 <maria.bilous101@gmail.com>
2019-04-07 20:15:07 +0300

    3rd method

--- src/edx/module1_javabasics_project/TripPlanner.java
+++ src/edx/module1_javabasics_project/TripPlanner.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TripPlanner {
 
-    public static void greet(){
+    public static void greet() {
         System.out.println("Welcome to Vacation Planner!");
         System.out.print("What is your name?\t");
         Scanner scanner = new Scanner(System.in);
@@ -26,18 +26,39 @@ public class TripPlanner {
         System.out.print("How many " + curr_sign + " are there in one USD?\t");
         double ex_rate = scanner.nextDouble();
         System.out.println();
-        System.out.println("If you are travelling for " + days + " days" + " that is the same as " + days*24 +
-                " hours " + "or " + days*24*60 + " minutes");
+        System.out.println("If you are travelling for " + days + " days" + " that is the same as " + days * 24 +
+                " hours " + "or " + days * 24 * 60 + " minutes");
         System.out.println("If you are going to spend " + "$" + sum + " USD" +
-                " that means per day you can spend up to " + "$" + (double)sum/days + " USD");
-        System.out.println("Your total budget in " + curr_sign + " is " + sum*ex_rate + " " + curr_sign +
-                ", which per day is " + sum*ex_rate/days + " " + curr_sign);
+                " that means per day you can spend up to " + "$" + (double) sum / days + " USD");
+        System.out.println("Your total budget in " + curr_sign + " is " + sum * ex_rate + " " + curr_sign +
+                ", which per day is " + sum * ex_rate / days + " " + curr_sign);
         System.out.println("**********");
 
     }
 
+    public static void time_difference() {
+        System.out.print("What is the time difference, in hours, between your home and your destination\t");
+        Scanner scanner = new Scanner(System.in);
+        int time_diff = scanner.nextInt();
+
+        while (time_diff >= 12 || time_diff <= -12){
+            System.out.println("The number must be within -12 and 12");
+            System.out.print("What is the time difference, in hours, between your home and your destination?\t");
+            time_diff = scanner.nextInt();
+        }
+
+        if (time_diff >= 0 && time_diff <= 12) {
+            System.out.println("That means that when it is midnight at home it will be " + time_diff + ":00 " +
+                    "in your travel destination and when it is noon at home it will be " + (12 + time_diff) + ":00");
+        }else if (time_diff < 0 && time_diff >= -12){
+            System.out.println("That means that when it is midnight at home it will be " + (24 + time_diff) +":00 " +
+                    "in your travel destination and when it is noon at home it will be " + (12 + time_diff) + ":00");
+        }
+    }
+
     public static void main(String[] args) {
 //        greet();
-        timing_and_budgeting();
+//        timing_and_budgeting();
+        time_difference();
     }
 }
